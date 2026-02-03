60917cdfd632b7a2c37af42e51489f7a504efda0
Mushkan  Pandey <mushkanpandey501@gmail.com>
2026-02-03 17:46:24 +0000

    changes reuired

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  base: '/form-builder/',
+  build: {
+    outDir: 'docs',
+  },
+  plugins: [react()],
+
 })
